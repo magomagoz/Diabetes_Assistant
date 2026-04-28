37a7092c9898243ee1b65cf325d2c2ac9dfbde96
magomagoz <magostinienrico@gmail.com>
2026-04-28 19:57:02 +0200

    Update DA.py

--- DA.py
+++ DA.py
@@ -4,7 +4,7 @@ from PIL import Image
 import os
 
 # Configurazione della pagina Streamlit
-st.set_page_config(page_title="Diabete-IA Helper", layout="Wide")
+st.set_page_config(page_title="Diabete-IA Helper", layout="Centered")
 st.title("🥗 Diabetes Assistant")
 st.write("Carica la foto del tuo piatto per una stima dei carboidrati (CHO).")
 
